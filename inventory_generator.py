f731a25bf77da6b2d1caceed518fe0e47020cae7
Andrés C <39702692+ac-crystal@users.noreply.github.com>
2022-07-10 18:27:45 -0500

    Completed generator for the inventory file

--- inventory_generator.py
+++ inventory_generator.py
@@ -1,4 +1,4 @@
-import os, time
+import os, json, time
 import requests
 
 from bs4 import BeautifulSoup
@@ -15,10 +15,18 @@ def recent_cache(file):
     dt = time.time() - os.path.getmtime(file)
     #more recent than a week
     return dt < 7*24*3600
+
+def save_json(data, dfile):
+    json_text = json.dumps(data)
+    with open(dfile, 'w', encoding="utf-8") as ofile:
+        ofile.write(json_text)
+
 # https://unicode.org/emoji/charts/full-emoji-list.html
 # https://www.unicode.org/emoji/charts-15.0/emoji-ordering.html
+# https://www.unicode.org/cldr/cldr-aux/charts/34/supplemental/territory_subdivisions.html#cosap
 
 def process_inventory_file(fname):
+    inventory = []
     with open(fname, encoding="utf-8") as ifile:
         ftext = ifile.read()
         soup = BeautifulSoup(ftext, 'html.parser')
@@ -27,12 +35,21 @@ def process_inventory_file(fname):
             contents = row.find_all(recursive=False)
             if len(contents)==1:
                 head = contents[0]
-                print(head.attrs['class'][0])
-                print(head.get_text())
+                classes = head.attrs['class']
+                name = head.get_text()
+                if 'bighead' in classes:
+                    citems = []
+                    inventory.append({'name':name, 'items':citems})
+                elif 'mediumhead' in classes:
+                    scitems = []
+                    citems.append({'name':name, 'items':scitems})
             else:
                 if contents[0].name=='th':
                     continue
-                print((contents[2].get_text(),contents[-1].get_text()))
+                #print((contents[2].get_text(),contents[-1].get_text()))
+                scitems.append({'text':contents[2].get_text(), 'cldr':contents[-1].get_text()})
+    #print(inventory)
+    return inventory
 
 def generate_inventory_from_list():
     url = 'https://unicode.org/emoji/charts/full-emoji-list.html'
@@ -42,6 +59,11 @@ def generate_inventory_from_list():
         download_page(url,cfile)
     else:
         print("Inventory cache found")
-    process_inventory_file(cfile)
+    data = process_inventory_file(cfile)
+    for i in data:
+        print(i['name'])
+        for j in i['items']:
+            print("  %s (%d) " % (j['name'],len(j['items'])))
+    save_json(data,"data_inventory.json")
 
 generate_inventory_from_list()
